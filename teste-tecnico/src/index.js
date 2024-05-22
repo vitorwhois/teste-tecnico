5d177bf82c4540491368a46e3c1625d6ad6665da
Vitor Ruis <108823068+vitorwhois@users.noreply.github.com>
2024-05-22 02:29:53 -0300

    git ignore

--- teste-tecnico/src/index.js
+++ teste-tecnico/src/index.js
@@ -1,13 +1,11 @@
 const path = require('path'); // Importa o módulo 'path' do Node.js para manipulação de caminhos
 const fs = require('fs'); // Importa o módulo 'fs' do Node.js para manipulação de arquivos
 
-const lerCSV = require('./csvReader'); // Importa a função lerCSV do módulo csvReader.js
-const { filtrarPessoasMaiores18, ordenarPessoasPorNome } = require('./dataProcessor'); // Importa as funções de processamento de dados
-const escreverArquivoTXT = require('./fileWriter'); // Importa a função escreverArquivoTXT
+const lerCSV = require('./csvReader'); 
+const { filtrarPessoasMaiores18, ordenarPessoasPorNome } = require('./dataProcessor'); 
+const escreverArquivoTXT = require('./fileWriter'); 
 
-/**
- * Função principal para executar o script.
- */
+
 async function main() {
     const [caminhoCSV, nomeArquivoTXT] = process.argv.slice(2);
 
@@ -24,7 +22,7 @@ async function main() {
         // Define o caminho de saída para o arquivo TXT
         const caminhoTXT = path.join(__dirname, 'data', 'output', nomeArquivoTXT);
 
-        // Ensure the output directory exists
+        // Confirma se o diretorio output  existe
         const outputDir = path.join(__dirname, 'data', 'output');
         if (!fs.existsSync(outputDir)) {
             fs.mkdirSync(outputDir, { recursive: true });
